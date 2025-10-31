0c8a7df7a54cb4a80b4f9db51496bae2e8a4919a
unknown <magnodsrr@gmail.com>
2025-10-31 19:28:47 -0300

    Iniciando nivel novato

---
+++ desafios/war/novato.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+/* 
+Criação da struct: definir uma struct chamada Territorio com os campos char nome[30], char cor[10] e int tropas.
+*/
+
+
+typedef struct {
+    char nome[30];
+    char cor[10];
+    int tropas;
+} Territorio;
+
+
+Territorio* CadastrarTerritorio(int totalTerritorios) ;
+void MostrarTerritorios(Territorio* territorios, int totalTerritorios);
+
+
+
+int main() {
+   printf("Bem vindo ao jogo War!\n");
+    int totalTerritorios = 0;
+
+    printf("Quantos territorios você deseja cadastrar?");
+    scanf("%d", &totalTerritorios);
+
+    Territorio* territoriosCadastrados = CadastrarTerritorio(totalTerritorios);
+
+    MostrarTerritorios(territoriosCadastrados, totalTerritorios);
+
+   return 0;
+}
+
+
+
+/*
+Cadastro dos territórios: o sistema deve permitir que o usuário cadastre cinco territórios informando o nome, cor do exército e o número de tropas de cada um.
+*/
+
+Territorio* CadastrarTerritorio(int totalTerritorios) {
+    Territorio* territorios;
+
+    territorios = (Territorio*)malloc(totalTerritorios * sizeof(Territorio));
+
+    for (int i = 0; i < totalTerritorios; i++) {
+        
+        printf("Digite o nome do %dº/%d território: ",i+1, totalTerritorios);
+        scanf("%s", territorios[i].nome);
+        printf("Digite a cor do exército do %dº/%d território: ",i+1, totalTerritorios);
+        scanf("%s", territorios[i].cor);
+        printf("Digite o número de tropas do %dº/%d território: ",i+1, totalTerritorios);
+        scanf("%d", &territorios[i].tropas);
+    }
+
+    return territorios;
+}
+
+void MostrarTerritorios(Territorio* territorios, int totalTerritorios) {
+    for (int i = 0; i < totalTerritorios; i++) {
+        printf("Território %d:\n", i + 1);
+        printf("Nome: %s\n", territorios[i].nome);
+        printf("Cor do Exército: %s\n", territorios[i].cor);
+        printf("Número de Tropas: %d\n\n", territorios[i].tropas);
+    }
+}
+
+
+
+
+
+
+
+   
